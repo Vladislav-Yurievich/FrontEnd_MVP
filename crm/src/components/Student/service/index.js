42fe8d32f4c3e7f0cd3c8874414877b203955ed4
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-10-12 01:25:15 +0500

    add get-me and update get-scores methods

--- crm/src/components/Student/service/index.js
+++ crm/src/components/Student/service/index.js
@@ -2,14 +2,20 @@ import { $authApi } from "../../../api"
 
 
 class StudentService {
+    getMe = async () => {
+        const response = await $authApi.get('/me');
+        return response;
+    }
+
     getCourses = async () => {
         const response = await $authApi.get('/get-my-courses');
         return response;
     }
 
     //! пока нет обработки на беке
-    getScores = async () => {
-        const response = await $authApi.get('/get-my-scores');
+    getScores = async (course_content_id) => {
+        const student_id = (await this.getMe()).data.id;
+        const response = await $authApi.get(`/get-scores?student_id=${student_id}&course_content_id=${course_content_id}`);
         return response;
     }
 }
