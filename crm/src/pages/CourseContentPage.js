4205266a0f2c8e160b289d7348702eb9d4b6ca4d
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-10-12 01:25:25 +0500

    add pages

---
+++ crm/src/pages/CourseContentPage.js
@@ -0,0 +1,10 @@
+import CourseContent from "../components/Student/CourseContent";
+
+
+const CourseContentPage = () => {
+    return (
+        <CourseContent />
+    );
+}
+
+export default CourseContentPage;
