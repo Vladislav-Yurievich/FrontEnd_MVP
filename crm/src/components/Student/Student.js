5ed249ed0be0e0cb5998923341a3383b28eb8aef
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-10-12 01:27:35 +0500

    ustal pisat' commits, update student and grade

--- crm/src/components/Student/Student.js
+++ crm/src/components/Student/Student.js
@@ -1,37 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import SideBar from './SideBar';
-import { studentService } from './service';
-import { useFetching } from '../../hooks/useFetching';
-import CoursesList from '../UI/CoursesList';
 import { titles } from './config';
-
-
-const TEMP_COURSES = [
-    { course_content_id: 4, course_content_name: 'Программирование в очке' },
-    { course_content_id: 24, course_content_name: 'МАТЕМАТИКА в МАЙНКРАФТ' },
-    { course_content_id: 12, course_content_name: 'Шахтерские переговоры в тоннелях!' },
-    { course_content_id: 7, course_content_name: 'АСОиУ-22 ИИПБ-22 Учебный план' },
-    { course_content_id: 456, course_content_name: 'Вязание шапочек и шарфиков' },
-    { course_content_id: 90, course_content_name: 'Выращивание травки в домашних условиях' },
-    { course_content_id: 13, course_content_name: 'Тринацать' },
-];
+import BuisnessCoursesList from './BuisnessCoursesList';
 
 
 const Student = () => {
-    const [courses, setCourses] = useState(null);
-
-    const [getCourses, loadingCourses, errorCourses] = useFetching(async () => {
-        // пока выводятся временные значения, потому что у меня пустая бд :P
-        // const courses = await studentService.getCourses();
-        // console.log(courses.data);
-        setCourses(TEMP_COURSES);
-    });
-
-    useEffect(() => {
-        getCourses();
-    }, []);
-
-
     return (
         <div className="container-fluid">
             <div className="row flex-nowrap">
@@ -43,7 +16,7 @@ const Student = () => {
                             {titles.current_courses}
                         </p>
 
-                        {courses ? <CoursesList courses={courses} /> : titles.loading}
+                        <BuisnessCoursesList link={'student/course'} />
                     </div>
                 </div>
             </div>
