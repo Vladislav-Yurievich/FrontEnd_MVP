17fdc47018a620ab06698984cedf8a91c986e3b7
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-10-13 00:38:49 +0500

    fixs

--- crm/src/api/index.js
+++ crm/src/api/index.js
@@ -1,41 +1,21 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 const API_URL = process.env.REACT_APP_API_URL;
 
 // для обычных звпросов
 
-=======
-
-const API_URL = process.env.REACT_APP_API_URL;
-
-// для обычных запросов
->>>>>>> 5ed249ed0be0e0cb5998923341a3383b28eb8aef
 export const $api = axios.create({
     baseURL: API_URL,
     withCredentials: true,
 });
 
-<<<<<<< HEAD
 // для запросов где нужна информация по токену
-export const $authAPI = axios.create({
-=======
-// для запросов где нужна антификация по токену 
 export const $authApi = axios.create({
->>>>>>> 5ed249ed0be0e0cb5998923341a3383b28eb8aef
     baseURL: API_URL,
     withCredentials: true,
 });
 
-<<<<<<< HEAD
-$authAPI.interceptors.request.use(config => {
-    config.headers.AccessToken = `${localStorage.getItem('UserToken')}`
-    return config;
-});
-=======
 $authApi.interceptors.request.use(config => {
     config.headers.AccessToken = `${localStorage.getItem('UserToken')}`
     return config;
 });
-
->>>>>>> 5ed249ed0be0e0cb5998923341a3383b28eb8aef
