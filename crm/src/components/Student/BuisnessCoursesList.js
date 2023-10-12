61412b945304fd004cf9eb87657cc96aa80e3fa9
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-10-13 00:45:35 +0500

    change temp_corses data

--- crm/src/components/Student/BuisnessCoursesList.js
+++ crm/src/components/Student/BuisnessCoursesList.js
@@ -6,12 +6,9 @@ import { titles } from './config';
 
 
 const TEMP_COURSES = [
-    { course_content_id: 4, course_content_name: 'Программирование в очке' },
-    { course_content_id: 24, course_content_name: 'МАТЕМАТИКА в МАЙНКРАФТ' },
-    { course_content_id: 12, course_content_name: 'Шахтерские переговоры в тоннелях!' },
+    { course_content_id: 4, course_content_name: 'Программирование' },
+    { course_content_id: 24, course_content_name: 'МАТЕМАТИКА' },
     { course_content_id: 7, course_content_name: 'АСОиУ-22 ИИПБ-22 Учебный план' },
-    { course_content_id: 456, course_content_name: 'Вязание шапочек и шарфиков' },
-    { course_content_id: 90, course_content_name: 'Выращивание травки в домашних условиях' },
     { course_content_id: 13, course_content_name: 'Тринацать' },
 ];
 
